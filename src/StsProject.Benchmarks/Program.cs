98e9eaec37ba6e5ea3c14267b7d682296bc6ffd1
James Ko <jamesqko@gmail.com>
2018-02-01 22:39:47 -0500

    Delete bs

--- src/StsProject.Benchmarks/Program.cs
+++ src/StsProject.Benchmarks/Program.cs
@@ -7,33 +7,9 @@ namespace StsProject.Benchmarks
 {
     class Program
     {
-        static int N = 50000;
-
         static void Main(string[] args)
         {
-            //var summary = BenchmarkRunner.Run<ListVsGrowthArray>();
-            Debugger.Break();
-            GrowthArray(out var collection);
-            Debugger.Break();
-            GC.KeepAlive(collection);
-        }
-
-        static void List(out List<object> collection)
-        {
-            collection = new List<object>();
-            for (int i = 0; i < N; i++)
-            {
-                collection.Add(null);
-            }
-        }
-
-        static void GrowthArray(out GrowthArray<object> collection)
-        {
-            collection = new GrowthArray<object>();
-            for (int i = 0; i < N; i++)
-            {
-                collection.Append(null);
-            }
+            var summary = BenchmarkRunner.Run<ListVsGrowthArray>();
         }
     }
 }
